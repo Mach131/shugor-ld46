554ca8df0cf7e31c690b166f8f7a102f0487bcd0
Temi Taylor <temit@mit.edu>
2020-04-18 23:05:17 -0400

    Finishing implementation of basic feeding interaction

--- Assets/Scripts/FoodPieceScript.cs
+++ Assets/Scripts/FoodPieceScript.cs
@@ -11,7 +11,10 @@ public class FoodPieceScript : MonoBehaviour
     private float healthRestoreAmount = 10;
     private float needinessIncreaseAmount = 10;
     private PetStatus petStatus;
+    private Collider petColl;
     private Camera sceneCamera;
+
+    private Collider coll;
 
     private void Update()
     {
@@ -30,18 +33,32 @@ public class FoodPieceScript : MonoBehaviour
         this.needinessIncreaseAmount = needinessIncreaseAmount;
         this.petStatus = petStatus;
         this.sceneCamera = sceneCamera;
+
+        this.petColl = petStatus.GetComponentInChildren<Collider>();
+        this.coll = GetComponentInChildren<Collider>();
     }
 
     // Called when the mouse button is released
     public void releaseFoodPiece()
     {
-        //TODO: check if overlapping the pet, restore hunger if so
-        Debug.Log("released");
+        if (checkPetOverlap())
+        {
+            this.petStatus.increaseHealth(healthRestoreAmount);
+            this.petStatus.increaseNeediness(needinessIncreaseAmount);
+            Debug.Log("yum");
+        }
         GameObject.Destroy(gameObject);
     }
 
-    private void checkPetOverlap()
+    private bool checkPetOverlap()
     {
-        Collider petBounds = petStatus.GetComponentInChildren<Collider>();
+        Rect petBoundsRect = boundsToRect(petColl.bounds);
+        Rect foodBoundsRect = boundsToRect(coll.bounds);
+        return petBoundsRect.Overlaps(foodBoundsRect);
+    }
+
+    private Rect boundsToRect(Bounds bounds)
+    {
+        return new Rect(bounds.min, bounds.size);
     }
 }
