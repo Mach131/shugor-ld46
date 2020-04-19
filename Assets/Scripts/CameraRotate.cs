39553e23f28fd8295d9a5cf91f81d22ae4da4023
samdarlington03 <37090767+samdarlington03@users.noreply.github.com>
2020-04-19 21:23:07 +1200

    Fixed Visual Error

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraRotate : MonoBehaviour
 {
 	public Transform MainCam; //Main Camera
-	public float MovementDistance=3.0f; //How many degrees out the camera is allowed to move.
+	public float MovementDistance=3.0f; //How many degrees out the camera is allowed to move
+	public float movementRestriction = 0.9f;
 	private Quaternion startAngle; //Original Position of Camera
 	private Vector3 startPos;
     // Start is called before the first frame update
@@ -23,6 +24,6 @@ public class CameraRotate : MonoBehaviour
     	var MouseOffsetX = Mathf.Clamp((RealMouseX-(Screen.width/2))/Screen.width,-1,1); //Convert Mouse X to value between -1 and 1 where 0 is the center.
     	var MouseOffsetY = Mathf.Clamp((RealMouseY-(Screen.height/2))/Screen.height,-1,1); //Convert Mouse Y to value between -1 and 1 where 0 is the center.
         MainCam.rotation = Quaternion.Slerp(MainCam.rotation, startAngle*Quaternion.Euler( MouseOffsetY*MovementDistance, -MouseOffsetX*MovementDistance, 0.0f),0.2f); // Set the rotation to the relative offset.
-        MainCam.position = Vector3.Lerp(MainCam.position, startPos + new Vector3(MouseOffsetX, MouseOffsetY,0.0f),0.2f);
+        MainCam.position = Vector3.Lerp(MainCam.position, startPos + new Vector3(MouseOffsetX*movementRestriction, MouseOffsetY*movementRestriction,0.0f),0.2f);
     }
 }
