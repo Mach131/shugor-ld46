d336fa5316d0b766467850d8a77bc47673838123
Temi Taylor <temit@mit.edu>
2020-04-19 04:02:27 -0400

    Starting to work on an interface for interaction systems

---
+++ Assets/Scripts/InteractionData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An interface for some functionality that interaction systems should be able
+/// to handle. For reference, a single "interaction system" refers to the components
+/// involved in maintaining a single one of the pet's conditions.
+/// </summary>
+public interface InteractionData
+{
+    /// <summary>
+    /// Adds the condition that this interaction system is connected to to the
+    /// petStatus object.
+    /// </summary>
+    /// <param name="petStatus">Pet's status object</param>
+    void addCondition(PetStatus petStatus);
+
+    /// <summary>
+    /// Instantiates all of the objects required for this interaction system to work.
+    /// </summary>
+    void instantiateInteractions();
+
+    // Can add other things that all interaction systems should have later; for
+    // instance, maybe a way to update neediness influences
+}
