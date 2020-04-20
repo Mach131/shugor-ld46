af3c3b230db2d5bf65a83cb750d96c5c2440a5d4
Temi Taylor <temit@mit.edu>
2020-04-20 19:31:43 -0400

    Adding sound effect files

---
+++ Assets/Scripts/SoundEffectHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectHandler : MonoBehaviour
+{
+    public static SoundEffectHandler player;
+
+    public AudioSource soundEffectSource;
+    public PetFormController petForm;
+
+    [Serializable]
+    public struct PetFormSfx
+    {
+        public List<AudioClip> neutralSoundEffects;
+        public List<AudioClip> upsetSoundEffects;
+    }
+    [SerializeField]
+    public List<PetFormSfx> petFormSoundEffects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundEffectHandler.player = this;
+    }
+
+    public void playRandomNeutralSound()
+    {
+        if (!soundEffectSource.isPlaying)
+        {
+            int petFormIndex = petForm.getPetFormIndex();
+            List<AudioClip> soundEffectOptions =
+                petFormSoundEffects[petFormIndex].neutralSoundEffects;
+            int randomIndex = UnityEngine.Random.Range(0, soundEffectOptions.Count);
+            AudioClip newEffect = soundEffectOptions[randomIndex];
+
+            soundEffectSource.PlayOneShot(newEffect);
+        }
+    }
+}
