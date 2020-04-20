47f48aea7a25ac19b86ba343c3d7882367a1ade6
samdarlington03 <37090767+samdarlington03@users.noreply.github.com>
2020-04-21 11:15:06 +1200

    Fade In Object

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+
+	private Renderer render;
+	private Color DarkColor;
+	private float LerpAlpha=1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        DarkColor=render.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LerpAlpha=Mathf.Lerp(LerpAlpha,-1,0.05f);
+        render.material.SetColor("_Color", new Color(DarkColor.r,DarkColor.g,DarkColor.b,LerpAlpha));
+        if (LerpAlpha<-0.75f)
+        {
+        	gameObject.SetActive(false);
+        }
+    }
+}
